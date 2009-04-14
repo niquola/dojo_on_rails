67e3759b1bb48c281c3148045200db542a5de0e2
nicola <nicola@niquola@gmail.com>
2009-04-14 18:38:08 +0400

    remove trash

--- installation_resources/dojo_root/src/kley/mvc/MediatorMixin.js
+++
@@ -1,8 +0,0 @@
-dojo.provide("kley.mvc.MediatorMixin");
-dojo.declare('kley.mvc.MediatorMixin', null, {
-  publish: function() {
-    var opts = dojo._toArray(arguments);
-    var topic = opts.shift();
-    dojo.publish(topic, opts);
-  }
-});
